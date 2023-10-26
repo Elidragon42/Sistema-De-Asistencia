568c6d0543022438b02a06781a636716d365b559
Elias Jaimes <joabsito1724@gmail.com>
2023-10-26 09:33:21 -0700

    Middleware Laravel10

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,21 @@ use Illuminate\Support\Facades\Route;
 
 route::view('/','welcome')->name('welcome');
 
-Route::get('/chirp', function () {
-    return 'welcome to our chirps page';
-})->name('chirps.index');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
+    route::view('/dashboard','dashboard')->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/chirps', function () {
+    return 'welcome to our chirps page';
+})->name('chirps.index');
+
 });
 
 require __DIR__.'/auth.php';
